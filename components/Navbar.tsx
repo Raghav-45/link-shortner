2b76a15f9194b7a5792f43b96ac87dbff96af129
Raghav-45 <raghavbhai4545@gmail.com>
2023-07-22 20:30:19 +0530

    url

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon, SunIcon, MoonIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
@@ -15,7 +15,7 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-black">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -60,10 +60,11 @@ export default function Navbar() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  className="rounded-full bg-white/10 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  {/* <SunIcon className="h-6 w-6" aria-hidden="true" /> */}
+                  <MoonIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
                 {/* Profile dropdown */}
---
+++ pages/file/[linkId].tsx
@@ -0,0 +1,55 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+const linkId = () => {
+  const router = useRouter()
+  // const [isLoading, setIsLoading] = useState<string>('null')
+  const [foundLink, setFoundLink] = useState<string | null>()
+
+  interface ShortLink {
+    id: string;
+    url: string;
+  }
+
+  const gg: ShortLink[] = [
+    {id: 'shadjk1', url: 'google1.com'},
+    {id: 'shadjk2', url: 'google2.com'},
+    {id: 'shadjk3', url: 'google3.com'},
+    {id: 'shadjk4', url: 'google4.com'},
+    {id: 'shadjk5', url: 'google5.com'},
+    {id: 'shadjk6', url: 'google6.com'},
+    {id: 'shadjk7', url: 'google7.com'},
+    {id: 'shadjk8', url: 'google8.com'},
+  ]
+
+  function getUrlById(id: string): string | null {
+    const matchingItem = gg.find(item => item.id === id)
+    return matchingItem ? matchingItem.url : null
+  }
+
+  useEffect(() => {
+    if (router.query.linkId) {
+      // setIsLoading('true')
+      const link = getUrlById(router.query.linkId as string)
+      if (!link) {
+        // setIsLoading('false')
+        setFoundLink(null)
+        return
+      }
+      // setIsLoading('false')
+      setFoundLink(link)
+    }
+  }, [router])
+  
+  return (
+    <>
+      {/* {isLoading == 'null' && <div>init...</div>} */}
+      {/* {isLoading == 'true' && <div>loading...</div>} */}
+      {/* {isLoading == 'false' && <div>ab ni kr rha...</div>} */}
+      {!foundLink && <div>url not found</div>}
+      {foundLink && <div>url mil gya</div>}
+    </>
+  )
+}
+
+export default linkId
