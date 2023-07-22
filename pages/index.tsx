40aabaf4979fd6a600eef89f9d7a46c1abe44b05
Raghav-45 <raghavbhai4545@gmail.com>
2023-07-22 20:37:01 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import Navbar from '@/components/navbar'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
