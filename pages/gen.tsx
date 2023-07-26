d00792e03b13ac508c7f226c3ab66e5178366034
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-26 15:15:06 +0530

    Update gen.tsx

--- pages/gen.tsx
+++ pages/gen.tsx
@@ -8,13 +8,13 @@ import { useRouter } from 'next/router'
 
 const GeneratorPage = () => {
   const router = useRouter()
-  const getBaseUrl = () => typeof window !== 'undefined' ? window.location.origin : ''
-  const baseURL = getBaseUrl()
 
   const [urlToShorten, setUrlToShorten] = useState('')
   const [shortLink, setShortLink] = useState('')
 
-  const isValidUrl = (urlString: string) => {
+  const baseURL = () => typeof window !== 'undefined' ? window.location.origin : ''
+
+  const isValidUrl = (urlString: string): boolean => {
     try { 
       return Boolean(new URL(urlString))
     }
@@ -28,13 +28,13 @@ const GeneratorPage = () => {
       addDoc(collection(db, "Links"), {
         url: urlToShorten,
         // timestamp: '',
-      }).then((docRef) => {setShortLink(docRef.id)})
-    } else {
-      toast.error('Oops! It seems like you entered an invalid URL.', {
-        style: {
-          width: 'auto',
-        },
+      }).then((docRef) => {
+        setShortLink(docRef.id)
+        toast.success('Success! Thank you for using our service!')
+        console.log('Success! Thank you for using our service!')
       })
+    } else {
+      toast.error('Oops! It seems like you entered an invalid URL.')
       console.log('Oops! It seems like you entered an invalid URL.')
     }
   }
