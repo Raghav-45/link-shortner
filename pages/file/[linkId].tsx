79a80bb59fa4ab54681e00b5d27dae7ee65f6193
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-26 18:15:41 +0530

    Update [linkId].tsx

--- pages/file/[linkId].tsx
+++ pages/file/[linkId].tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 
 import { doc, getDoc } from "firebase/firestore"
 import { db } from '@/lib/firebaseClient'
+import { Button } from '@/components/ui/button'
 
 const LinkPage = () => {
   const router = useRouter()
@@ -49,7 +50,7 @@ const LinkPage = () => {
         const link = await getUrlById(router.query.linkId as string)
         setIsLoading(false)
         setFoundLink(link || null)
-        link && externalLinkRedirect(link)
+        // link && externalLinkRedirect(link)
         console.log(link)
       }
     };
@@ -59,11 +60,33 @@ const LinkPage = () => {
   return (
     <>
       <Navbar />
-      <div className='m-4'>
+      <div className='min-h-screen h-full w-full p-8'>
+        <div className='bg-black/5 min-h-[500px] h-full w-full rounded-2xl p-6'>
+          <h3 className="mb-3 break-all text-2xl font-semibold leading-none tracking-tight text-gray-900 dark:text-white">{"MimpsbeDd.108p_word4ufree.hair.mkv"}</h3>
+          <div>
+            <p className='inline-block font-bold'>size:</p>
+            <p className='inline-block uppercase'>{"5GB"}</p>
+          </div>
+          <div>
+            <p className='inline-block font-bold'>downloads:</p>
+            <p className='inline-block'>{"1250"}</p>
+          </div>
+          <div className='mt-6 flex items-center justify-center h-[80px] bg-yellow-300'>
+            <p className='text-xl uppercase'>#ads</p>
+          </div>
+          <div className='w-full my-4 text-center'>
+            <Button>Downlaod</Button>
+          </div>
+          <div className='mt-6 flex items-center justify-center h-[80px] bg-yellow-300'>
+            <p className='text-xl uppercase'>#ads</p>
+          </div>
+        </div>
+      </div>
+      {/* <div className='m-4'>
         {isLoading && <p>loading...</p>}
         {!isLoading && !foundLink && <p>We&#39;re sorry, but the short link you&#39;ve entered does not exist on our server. Please ensure you&#39;ve entered the correct link or contact the link&#39;s creator for assistance.</p>}
         {!isLoading && foundLink && <p>You are being redirected to the link.<br/>If the page does not load within a few seconds, click here: <a href={foundLink}>{foundLink}</a></p>}
-      </div>
+      </div> */}
     </>
   )
 }
