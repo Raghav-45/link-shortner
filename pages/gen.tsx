0e5f6eda8fdc788314c8b34b68cf3413e2123236
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-26 15:51:37 +0530

    added shadcn ui

--- pages/gen.tsx
+++ pages/gen.tsx
@@ -5,6 +5,8 @@ import toast, { Toaster } from 'react-hot-toast'
 import { collection, addDoc } from 'firebase/firestore'
 import { db } from '@/lib/firebaseClient'
 import { useRouter } from 'next/router'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 const GeneratorPage = () => {
   const router = useRouter()
@@ -54,16 +56,13 @@ const GeneratorPage = () => {
       <Navbar />
       <div className='my-40'>
         <div className="bg-blue-50 flex items-center justify-center">
-          <div className="bg-white w-full max-w-md p-8 rounded-lg shadow-md">
+          <div className="bg-white w-full max-w-lg p-8 rounded-lg shadow-md">
             <h1 className="text-3xl font-bold mb-6 text-center">Link Shortener</h1>
-            <div className="flex flex-col">
-              <input value={urlToShorten} onChange={(e) => setUrlToShorten(e.target.value)} type="url" name="originalUrl" className="p-3 mb-4 border rounded-md focus:outline-none focus:ring focus:border-blue-500" placeholder="Enter your long URL" required />
-              <button onClick={() => updateLinkOnDB()} type="submit" className="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md focus:outline-none focus:ring focus:border-blue-500">
-                Shorten
-              </button>
+            <div className="flex w-full items-center space-x-2">
+              <Input value={urlToShorten} onChange={(e) => setUrlToShorten(e.target.value)} type="url" placeholder="Enter your long URL" required />
+              <Button onClick={() => updateLinkOnDB()}>Shorten</Button>
             </div>
-
-            {shortLink &&<div className="mt-6" id="shortenedUrlContainer">
+            {shortLink &&<div className="ml-1 mt-3">
               link generated = <a href={`${baseURL}/file/${shortLink}`}>follow Link</a>
             </div>}
           </div>
