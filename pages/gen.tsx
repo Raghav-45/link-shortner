9b225c3d2e2147dd3558f1a5fca9a7cdf16fcea5
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-26 16:38:36 +0530

    Update gen.tsx

--- pages/gen.tsx
+++ pages/gen.tsx
@@ -7,14 +7,17 @@ import { db } from '@/lib/firebaseClient'
 import { useRouter } from 'next/router'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Loader2 } from 'lucide-react'
 
 const GeneratorPage = () => {
   const router = useRouter()
 
+  const [isGenerating, setIsGenerating] = useState(false)
   const [urlToShorten, setUrlToShorten] = useState('')
   const [shortLink, setShortLink] = useState('')
 
-  const baseURL = () => typeof window !== 'undefined' ? window.location.origin : ''
+  const getBaseUrl = () => typeof window !== 'undefined' ? window.location.origin : ''
+  const baseURL = getBaseUrl()
 
   const isValidUrl = (urlString: string): boolean => {
     try { 
@@ -32,10 +35,13 @@ const GeneratorPage = () => {
         // timestamp: '',
       }).then((docRef) => {
         setShortLink(docRef.id)
+        setIsGenerating(false)
         toast.success('Success! Thank you for using our service!')
+        navigator.clipboard.writeText(`${baseURL}/file/${shortLink}`)
         console.log('Success! Thank you for using our service!')
       })
     } else {
+      setIsGenerating(false)
       toast.error('Oops! It seems like you entered an invalid URL.')
       console.log('Oops! It seems like you entered an invalid URL.')
     }
@@ -49,8 +55,11 @@ const GeneratorPage = () => {
           <div className="bg-white w-full max-w-lg p-8 rounded-lg shadow-md">
             <h1 className="text-3xl font-bold mb-6 text-center">Link Shortener</h1>
             <div className="flex w-full items-center space-x-2">
-              <Input value={urlToShorten} onChange={(e) => setUrlToShorten(e.target.value)} type="url" placeholder="Enter your long URL" required />
-              <Button onClick={() => updateLinkOnDB()}>Shorten</Button>
+              <Input value={shortLink.length != 0 ? shortLink : urlToShorten} onChange={(e) => {setShortLink(''); setUrlToShorten(e.target.value);}} type="url" placeholder="Enter your long URL" required />
+              <Button onClick={() => {setIsGenerating(true); updateLinkOnDB();}}>
+                {isGenerating && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                {shortLink ? 'Copied!' : 'Shorten'}
+              </Button>
             </div>
             {shortLink &&<div className="ml-1 mt-3">
               link generated = <a href={`${baseURL}/file/${shortLink}`}>follow Link</a>
