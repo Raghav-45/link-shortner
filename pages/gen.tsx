0693e44f9a1f63c0f68a5c58a7a54aab7c57cd9e
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-26 15:38:49 +0530

    Update gen.tsx

--- pages/gen.tsx
+++ pages/gen.tsx
@@ -40,14 +40,35 @@ const GeneratorPage = () => {
   }
 
   return (
+    // <>
+    //   <Navbar />
+    //   <input placeholder='url' value={urlToShorten} onChange={(e) => setUrlToShorten(e.target.value)} />
+    //   <button onClick={() => updateLinkOnDB()}>Generate</button>
+
+    //   {shortLink && <div>
+    //     link generated = <a href={`${baseURL}/file/${shortLink}`}>follow Link</a>
+    //   </div>}
+    //   <Toaster />
+    // </>
     <>
       <Navbar />
-      <input placeholder='url' value={urlToShorten} onChange={(e) => setUrlToShorten(e.target.value)} />
-      <button onClick={() => updateLinkOnDB()}>Generate</button>
+      <div className='my-40'>
+        <div className="bg-blue-50 flex items-center justify-center">
+          <div className="bg-white w-full max-w-md p-8 rounded-lg shadow-md">
+            <h1 className="text-3xl font-bold mb-6 text-center">Link Shortener</h1>
+            <div className="flex flex-col">
+              <input value={urlToShorten} onChange={(e) => setUrlToShorten(e.target.value)} type="url" name="originalUrl" className="p-3 mb-4 border rounded-md focus:outline-none focus:ring focus:border-blue-500" placeholder="Enter your long URL" required />
+              <button onClick={() => updateLinkOnDB()} type="submit" className="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md focus:outline-none focus:ring focus:border-blue-500">
+                Shorten
+              </button>
+            </div>
 
-      {shortLink && <div>
-        link generated = <a href={`${baseURL}/file/${shortLink}`}>follow Link</a>
-      </div>}
+            {shortLink &&<div className="mt-6" id="shortenedUrlContainer">
+              link generated = <a href={`${baseURL}/file/${shortLink}`}>follow Link</a>
+            </div>}
+          </div>
+        </div>
+      </div>
       <Toaster />
     </>
   )
